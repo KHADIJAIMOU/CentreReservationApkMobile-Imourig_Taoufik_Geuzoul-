49c472e31e91e89f7639bf13e2b63e4888cb8853
Zakia <zakiataou99@gmail.com>
2023-02-14 13:01:55 +0100

    datepicker + timepicker

--- app/src/main/java/com/example/centrereservation/controller/ReservationFragment.java
+++ app/src/main/java/com/example/centrereservation/controller/ReservationFragment.java
@@ -1,20 +1,29 @@
 package com.example.centrereservation.controller;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.example.centrereservation.R;
 
+import java.util.Calendar;
+
 public class ReservationFragment extends Fragment{
     Button btn;
+    EditText dateDebut, dateFin, tempsD,tempsF;
 
     public ReservationFragment() {
         // Required empty public constructor
@@ -29,6 +38,40 @@ public class ReservationFragment extends Fragment{
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reservation, container, false);
         btn= view.findViewById(R.id.btnreserver);
+        dateDebut = view.findViewById(R.id.editTextDateD);
+        dateFin = view.findViewById(R.id.editTextDateF);
+        tempsD = view.findViewById(R.id.editTextTempsD);
+        tempsF = view.findViewById(R.id.editTextTempsF);
+        dateDebut.setInputType(InputType.TYPE_NULL);
+        dateFin.setInputType(InputType.TYPE_NULL);
+        tempsD.setInputType(InputType.TYPE_NULL);
+        tempsF.setInputType(InputType.TYPE_NULL);
+
+        dateDebut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(dateDebut);
+            }
+        });
+
+        dateFin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(dateFin);
+            }
+        });
+        tempsD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(tempsD);
+            }
+        });
+        tempsF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(tempsF);
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +83,44 @@ public class ReservationFragment extends Fragment{
             }
         });
 
+
+
         return view;
+
+    }
+
+    private void showTimePickerDialog(EditText edt) {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                getContext(),
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        // Set the time to the EditText
+                        String time = hour + ":" + minute;
+                        edt.setText(time);
+                    }
+                },
+                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                Calendar.getInstance().get(Calendar.MINUTE),
+                false
+        );
+        timePickerDialog.show();
+
+    }
+
+    private void showDatePickerDialog(EditText edt) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String date = day + "/" + (month + 1) + "/" + year;
+                edt.setText(date);
+            }
+        }, year, month, day);
+        datePickerDialog.show();
     }
 }
